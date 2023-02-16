bdfd36681feabaaa36684e4e3a13f21647972d55
shivam <shivradhee@gmail.com>
2023-02-16 22:35:21 +0530

    fhasjhf

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,30 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthServiceService } from '../service/auth-service.service';
+// import { AuthServiceService } from '../service/auth-service.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  username!: string;
-  password!: string;
+  // username!: string;
+  // password!: string;
 
-  constructor(private authService:AuthServiceService) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
 
   
-  login() {
-    this.authService.login(this.username, this.password).subscribe(
-      response => {
-        console.log("successfully Enter Baby");
+  // login() {
+  //   this.authService.login(this.username, this.password).subscribe(
+  //     response => {
+  //       console.log("successfully Enter Baby");
         
-      },
-      error => {
-        console.log("Use Some Lubricant");
-      }
-    );
-  }
+  //     },
+  //     error => {
+  //       console.log("Use Some Lubricant");
+  //     }
+  //   );
+  // }
 
 }
